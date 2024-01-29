8355e7547fd7d293fa275d0f83c690c0edba303d
RohanRajendraDalvi <78230506+RohanRajendraDalvi@users.noreply.github.com>
2024-01-29 15:05:49 -0500

    older changes

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -1,9 +1,13 @@
 function setColor(set) {
         let changeColor = set;
-        if(changeColor) {
-            let userColor = document.getElementById('color').value;
-            document.getElementById('myPara').style.color = userColor;
+        return function() {
+            if(changeColor) {
+                let userColor = document.getElementById('color').value;
+                document.getElementById('myPara').style.color = userColor;
+                document.getElementById('myPara').style.fontSize = "XX-Large";
+            }
         }
+
         
 }
 
--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,12 @@
-// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg.
+
+
+let logMsg = function (){
+    console.log(this.errMsg);
+}
+
+const err = {
+    errMsg: "Baka!!!"
+}
+
+logMsg(logMsg.call(err));
